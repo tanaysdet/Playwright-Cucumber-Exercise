97df0ea761b92bfbe22e88b77c48ebf2dda099d8
tanaysdet <tanay.sdet@gmail.com>
2025-11-13 11:04:38 -0500

    Update product.steps.ts

--- steps/product.steps.ts
+++ steps/product.steps.ts
@@ -5,3 +5,15 @@ import { Product } from '../pages/product.page';
 Then('I will add the backpack to the cart', async () => {
   await new Product(getPage()).addBackPackToCart();
 });
+
+Then('I sort the products by {string}', async (sortOption) => {
+  await new Product(getPage()).sortProducts(sortOption);
+});
+
+Then('I should see the products sorted by price {string}', async (sortOption) => {
+  await new Product(getPage()).validatePriceSort(sortOption);
+});
+
+Then('I should see the products page title {string}', async (expectedTitle) => {
+  await new Product(getPage()).validateProductsPageTitle(expectedTitle);
+});
